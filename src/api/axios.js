86bc1826959e27a3dc2040d11bd3b713d81ce4a2
bokdol11859 <2019147551@yonsei.ac.kr>
2022-07-22 23:21:53 +0900

    banner component created with basic css

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-const REACT_APP_API_KEY = process.env;
-
-const instance = axios({
+const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: REACT_APP_API_KEY,
+    api_key: "c80c1b110217666d72db5bb84f7ad9d4",
     language: "ko-KR",
   },
 });
