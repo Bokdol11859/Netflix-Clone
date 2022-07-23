3c6798535d86444b0baed2a2118f6b3cfabf973e
bokdol11859 <2019147551@yonsei.ac.kr>
2022-07-23 23:54:01 +0900

    search page implemented

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 const Nav = () => {
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`search?q=${e.target.value}`);
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -24,10 +33,18 @@ const Nav = () => {
         alt="Netflix logo"
         className="nav__logo"
         onClick={() => {
-          window.location.reload();
+          navigate("/");
         }}
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
       />
+
+      <input
+        className="nav__input"
+        value={searchValue}
+        onChange={handleChange}
+        placeholder="제목, 사람, 장르"
+      />
+
       <img
         alt="user logged"
         className="nav__avatar"
--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,7 +1,78 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "../../api/axios";
+import "./SearchPage.css";
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const [searchResults, setSearchResults] = useState([]);
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(
+        `/search/multi?include_adult=true&query=${searchTerm}`
+      );
+      console.log(request);
+      setSearchResults(request.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const renderSearchResults = () => {
+    return searchResults.length > 0 ? (
+      <section className="search-container">
+        {searchResults.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== "person") {
+            const movieImageUrl =
+              "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+            return (
+              <div className="movie">
+                <div className="movie__column-poster">
+                  <img
+                    src={movieImageUrl}
+                    alt="movie image"
+                    className="movie__poster"
+                  />
+                </div>
+              </div>
+            );
+          }
+        })}
+      </section>
+    ) : (
+      <section className="no-results">
+        <div className="no-results__text">
+          <p>입력하신 검색어 "{searchTerm}"(와)과 일치하는 결과가 없습니다.</p>
+          <p>추천 검색어:</p>
+          <ul>
+            <li>다른 키워드를 입력해 보세요.</li>
+            <li>시리즈나 영화를 찾고 있으신가요?</li>
+            <li>
+              영화 제목, 시리즈 제목, 또는 배우나 감독의 이름으로 검색해 보세요.
+            </li>
+            <li>
+              코미디, 로맨스, 스포츠 또는 드라마와 같은 장르명으로 검색해
+              보세요.
+            </li>
+          </ul>
+        </div>
+      </section>
+    );
+  };
+
+  return renderSearchResults();
 };
 
 export default SearchPage;
