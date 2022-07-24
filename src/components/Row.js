02df79d9e802afda736006d46ec209972e862b5b
bokdol11859 <2019147551@yonsei.ac.kr>
2022-07-24 14:31:11 +0900

    movie rows swiper implemented with library

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,6 +2,13 @@ import axios from "../api/axios";
 import React, { useEffect, useState } from "react";
 import "./Row.css";
 import MovieModal from "./MovieModal/MovieModal";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const Row = ({ title, id, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
@@ -17,15 +24,6 @@ const Row = ({ title, id, fetchUrl, isLargeRow }) => {
     setMovies(request.data.results);
   };
 
-  const slide = (direction) => {
-    console.log(direction);
-    if (direction === "left") {
-      document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-    } else {
-      document.getElementById(id).scrollLeft += window.innerWidth + 80;
-    }
-  };
-
   const handleClick = (movie) => {
     setModalOpen(true);
     setMovieSelected(movie);
@@ -34,41 +32,50 @@ const Row = ({ title, id, fetchUrl, isLargeRow }) => {
   return (
     <section className="row">
       <h2>{title}</h2>
-      <div className="slider">
-        <div
-          className="slider__arrow-left"
-          onClick={() => {
-            slide("left");
-          }}
-        >
-          <span className="arrow">{"<"}</span>
-        </div>
 
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true}
+        navigation
+        pagination={{ clickable: true }}
+        breakpoints={{
+          1378: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+      >
         <div id={id} className="row__posters">
           {movies.map((movie) => (
-            <img
-              key={movie.id}
-              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-              src={`https://image.tmdb.org/t/p/original/${
-                isLargeRow ? movie.poster_path : movie.backdrop_path
-              }`}
-              alt={movie.name}
-              onClick={() => {
-                handleClick(movie);
-              }}
-            />
+            <SwiperSlide>
+              <img
+                key={movie.id}
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`https://image.tmdb.org/t/p/original/${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+                onClick={() => {
+                  handleClick(movie);
+                }}
+              />
+            </SwiperSlide>
           ))}
         </div>
+      </Swiper>
 
-        <div
-          className="slider__arrow-right"
-          onClick={() => {
-            slide("right");
-          }}
-        >
-          <span className="arrow">{">"}</span>
-        </div>
-      </div>
       {modalOpen && (
         <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
       )}
